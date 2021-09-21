0dbaa9c3a414a42a8589e716ea77a0ac45e0fdc4
Carbon32 <contact.carbonofficial@gmail.com>
2021-09-21 04:51:02 +0100

    Added functional buttons

--- main.py
+++ main.py
@@ -51,6 +51,8 @@ cannonBall = pygame.image.load('assets/Ball.png').convert_alpha()
 ballWidth = cannonBall.get_width()
 ballHeight = cannonBall.get_height()
 cannonBall = pygame.transform.scale(cannonBall, (int(ballWidth * 0.4), (int(ballHeight * 0.4))))
+repairButton = pygame.image.load('assets/Repair.png')
+armourButton = pygame.image.load('assets/Armour.png')
 enemyAnimations = []
 enemyTypes = ['Tank', 'Heavy',]
 enemyHealth = [50, 150]
@@ -89,7 +91,7 @@ def showStats():
 # Game Classes: #
 
 class Fort():
-    def __init__(self, image, secondImage, thirdImage, x, y, scale):
+    def __init__(self, firstImage, secondImage, thirdImage, x, y, scale):
         self.health = 1000
         self.maxHealth = self.health
         self.fired = False
@@ -97,7 +99,8 @@ class Fort():
         self.kills = 0
         width = image.get_width()
         height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.image = firstImage
+        self.firstImage = pygame.transform.scale(firstImage, (int(width * scale), int(height * scale)))
         self.secondImage = pygame.transform.scale(secondImage, (int(width * scale), int(height * scale)))
         self.thirdImage = pygame.transform.scale(thirdImage, (int(width * scale), int(height * scale)))
         self.rect = self.image.get_rect()
@@ -109,7 +112,7 @@ class Fort():
         xDistance = (position[0] - self.rect.midleft[0])
         yDistance = -(position[1] - self.rect.midleft[1])
         self.angle = math.degrees(math.atan2(yDistance, xDistance))
-        if (pygame.mouse.get_pressed()[0] and self.fired == False):
+        if (pygame.mouse.get_pressed()[0] and self.fired == False and position[1] > 100):
             ball = Ball(cannonBall, self.rect.midleft[0], self.rect.midleft[1], self.angle)
             cannonBalls.add(ball)
             self.fired = True
@@ -123,8 +126,21 @@ class Fort():
         elif(self.health <= 500):
             self.image = self.secondImage
         else:
-            self.image = self.image
+            self.image = self.firstImage
         gameWindow.blit(self.image, self.rect)
+
+    def repairFort(self):
+        if(self.money >= 500 and self.health < self.maxHealth):
+            self.health += 250
+            self.money -= 500
+            if (self.health > self.maxHealth):
+                self.health = self.maxHealth
+
+    def upgradeArmour(self):
+        if(self.money >= 1000):
+            self.maxHealth += 500
+            self.money -= 1000
+
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self, image, x, y, angle):
@@ -132,7 +148,7 @@ class Ball(pygame.sprite.Sprite):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = x
-        self.rect.y = y
+        self.rect.y = y+100
         self.angle = math.radians(angle)
         self.speed = 5
         self.deltaX = math.cos(self.angle) * self.speed
@@ -223,11 +239,36 @@ class Enemy(pygame.sprite.Sprite):
             self.frameIndex = 0
             self.updateTime = pygame.time.get_ticks()
 
+class Button():
+    def __init__(self, x, y, image, scale):
+        bWidth = image.get_width()
+        bHeight = image.get_height()
+        self.image = pygame.transform.scale(image, (int(bWidth * scale), int(bHeight * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    def drawButton(self):
+        action = False
+        position = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(position):
+            if(pygame.mouse.get_pressed()[0] == 1 and self.clicked == False):
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        gameWindow.blit(self.image, (self.rect.x, self.rect.y))
+        return action
 
 # Game Loop: #
 
-fort = Fort(fortUndamaged, fortDamaged, fortHeavilyDamaged, 500, 235, 3) # Fort Creation
+fort = Fort(fortUndamaged, fortDamaged, fortHeavilyDamaged, 500, 235, 4) # Fort Creation
 crosshair = Crosshair(1.5)
+buttonRepair = Button(700, -20, repairButton, 2)
+buttonArmour = Button(700, 20, armourButton, 2)
 cannonBalls = pygame.sprite.Group()
 gameEnemies = pygame.sprite.Group()
 
@@ -278,6 +319,11 @@ while gameRunning:
             gameEnemies.empty()
 
     showStats()
+    if(buttonRepair.drawButton()):
+        fort.repairFort()
+    if(buttonArmour.drawButton()):
+        fort.upgradeArmour()
+        pass
     # Events handler: #
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
